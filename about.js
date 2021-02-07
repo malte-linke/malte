61c1077528ef0605c643e5fdddd3eb7408da2853
Parzival <malte@parzival-dev.eu>
2021-02-07 21:45:34 +0100

    Initial commit

---
+++ about.js
@@ -0,0 +1,10 @@
+module.exports = {
+    name: "Malte Linke",
+    email: "malte.linke.ml@gmail.com",
+    title: "Self-taught Full Stack developer",
+    github: "malte-linke",
+    discord: "mxpyaBJefM",
+    twitter: "malte_linke",
+    contact: "http://social.parzival-dev.eu/",
+    urls: []
+}
